6eb9b69ee6ce8ee0b530052e8025e533e993675f
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-04-29 12:52:03 +0000

    Create main.go

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "net/http"
+    "os"
+    "strconv"
+    )
+
+
+func main () {
+    var port string
+    p := *flag.Int ("port", 0, "listening port")
+    if p == 0 {
+        port = os.Getenv ("PORT")
+        if len (port) < 1 {
+            port = "8080"
+        }
+    } else {
+        port = strconv.FormatInt (int64 (p), 10)
+    }
+    http.HandleFunc ("/hello/", hello)
+    fmt.Println ("Listening...")
+    err := http.ListenAndServe (":" + port, nil)
+    if err != nil {
+        panic (err)
+    }
+}
+
+func hello (w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln (w, "Hello, world")
+}

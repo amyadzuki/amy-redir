6a804d32f507ba6e2ec461517ef7859d23345d88
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-04-29 14:22:58 +0000

    Update main.go

--- main.go
+++ main.go
@@ -70,10 +70,10 @@ func PokemonSmol (w http.ResponseWriter, r *http.Request) {
     http.Redirect (w, r, url, http.StatusMovedPermanently)
 }
 
-const Pkmn = []string {
+var Pkmn = []string {
     "https://duckduckgo.com/assets/logo_icon128.v101.png", // test image
 }
 
-const PkmnSmol = []string {
+var PkmnSmol = []string {
     "https://duckduckgo.com/assets/logo_icon128.v101.png", // test image
 }

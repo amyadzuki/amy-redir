b3ad9dd57e6215101f4f95f0d24d7ef61ef38309
amyadzuki <31188054+amyadzuki@users.noreply.github.com>
2018-04-29 13:34:20 +0000

    Update main.go

--- main.go
+++ main.go
@@ -20,7 +20,11 @@ func main () {
     } else {
         port = strconv.FormatInt (int64 (p), 10)
     }
-    http.HandleFunc ("/", hello)
+    http.HandleFunc ("/",           Hello)
+    http.HandleFunc ("/ddgi/",      DDGTestImage)
+    http.HandleFunc ("/google/",    Google)
+    http.HandleFunc ("/pkmn/",      PokemonBig)
+    http.HandleFunc ("/pkmn/smol/", PokemonSmol)
     fmt.Println ("Listening...")
     err := http.ListenAndServe (":" + port, nil)
     if err != nil {
@@ -28,6 +32,26 @@ func main () {
     }
 }
 
-func hello (w http.ResponseWriter, r *http.Request) {
+func Hello (w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln (w, "Hello, world")
+}
+
+func DDGTestImage (w http.ResponseWriter, r *http.Request) {
+    http.Redirect (w, r, "https://duckduckgo.com/assets/logo_icon128.v101.png", http.StatusMovedPermanently)
+}
+
+func Google (w http.ResponseWriter, r *http.Request) {
+    http.Redirect (w, r, "https://google.com", http.StatusMovedPermanently)
+}
+
+func Obfuscation (w http.ResponseWriter, r *http.Request) {
+    http.Redirect (w, r, "https://google.com", http.StatusMovedPermanently)
+}
+
+func PokemonBig (w http.ResponseWriter, r *http.Request) {
+    http.Redirect (w, r, "https://google.com", http.StatusMovedPermanently)
+}
+
+func PokemonSmol (w http.ResponseWriter, r *http.Request) {
+    http.Redirect (w, r, "https://google.com", http.StatusMovedPermanently)
 }
